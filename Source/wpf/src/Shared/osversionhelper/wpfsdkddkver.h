3af74566747ae8a4bd7933dd40bb3e906eee37b3
fan yang <yfzc1526@outlook.com>
2019-04-03 15:19:35 +0800

    Signed-off-by: fan yang <yfzc1526@outlook.com>

---
+++ Source/wpf/src/Shared/osversionhelper/wpfsdkddkver.h
@@ -0,0 +1,10 @@
+#pragma once 
+#if !defined(_WPF_SDKDDKVER_H_)
+#define _WPF_SDKDDKVER_H_
+
+#define _TH1_BUILD_NUMBER                   10240
+#define _TH2_BUILD_NUMBER                   10586
+#define _RS1_BUILD_NUMBER                   14393
+#define _RS2_BUILD_NUMBER                   15063
+
+#endif // _WPF_SDKDDKVER_H_
